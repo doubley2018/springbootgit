f36f35a1fa1da9aaaba3e302e47c700db1b30f99
angi <657540264@qq.com>
2021-01-08 22:24:08 +0800

    1

---
+++ src/main/java/com/angi/Example.java
@@ -0,0 +1,25 @@
+package com.angi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author angi
+ * @date 2021/1/8 20:05
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+
+}

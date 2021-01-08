b1ebe211a96bd9aeff65b2a5b62b05d353df4c96
doubley2018 <73275951+doubley2018@users.noreply.github.com>
2021-01-09 00:01:12 +0800

    Update Example.java

--- src/main/java/com/angi/Example.java
+++ src/main/java/com/angi/Example.java
@@ -15,7 +15,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World git!";
     }
 
     public static void main(String[] args) {
